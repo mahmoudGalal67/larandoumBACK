7e5c4a25fc7e5fce18185d3daf1b966485693a75
Mahmoud Galal <mahmoudgalal69@gmail.com>
2023-02-12 13:12:04 +0200

    gerw

--- DB.js
+++ DB.js
@@ -1,16 +1,16 @@
 import mysql from "mysql";
 
-export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "seriallkiller",
-  database: "lerandom",
-});
-
-// export const db = mysql.createPool({
-//   connectionLimit: 100,
-//   host: "sql9.freesqldatabase.com",
-//   user: "sql9597044",
-//   password: "5H4BmcweNa",
-//   database: "sql9597044",
+// export const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "seriallkiller",
+//   database: "lerandom",
 // });
+
+export const db = mysql.createPool({
+  connectionLimit: 100,
+  host: "sql9.freesqldatabase.com",
+  user: "sql9597044",
+  password: "5H4BmcweNa",
+  database: "sql9597044",
+});
